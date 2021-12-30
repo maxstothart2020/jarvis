c38ec35bd4abe5248aaab04e5e4b462e41924481
maxstothart2020 <61262211+maxstothart2020@users.noreply.github.com>
2021-12-30 14:28:49 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,18 @@
 #!/bin/sh
+
+#Install Python Modules Using PIP
+pip install gtts
+pip install pygame
+pip install mutagen
+pip install speechrecognition
+
+#Install Python Support
+sudo apt install python3-pyaudio
+sudo apt install alsamixer
+
+#Finish Up
+chmod +x run.sh
 ls
+echo"Done"
+echo"Running Now"
+./run.sh
