df1f2271c4c2072228a3974a869f3d24963fe4ef
maxstothart2020 <maxstothart2020@gmail.com>
2022-01-11 23:45:54 +1300

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
+#install python and support
+echo "Installing Python and supports"
+sudo apt install python3 python3-pip python3-pyaudio alsamixer -y
+
 #Install Python Modules Using PIP
+echo "Installing python libaries using Pip"
 pip install gtts
 pip install pygame
 pip install mutagen
 pip install speechrecognition
 
-#Install Python Support
-sudo apt install python3-pyaudio
-sudo apt install alsamixer
-
 #Finish Up
+echo "Finishing up"
 chmod +x run.sh
 ls
 echo "Done"
